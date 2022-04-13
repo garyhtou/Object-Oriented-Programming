6ae8d8fece54c6b09a99f0eeac7996eb8a23c895
Gary Tou <gary@garytou.com>
2022-04-12 20:46:01 -0700

    Format GridFlea.cpp

--- P2/GridFlea.cpp
+++ P2/GridFlea.cpp
@@ -10,138 +10,113 @@
 #include <cmath>
 #include "GridFlea.h"
 
-GridFlea::GridFlea(int x, int y, unsigned int size, int reward, int energy)
-{
-	initX = x;
-	initY = y;
-	initEnergy = energy;
-	initReward = reward;
+GridFlea::GridFlea(int x, int y, unsigned int size, int reward, int energy) {
+    initX = x;
+    initY = y;
+    initEnergy = energy;
+    initReward = reward;
 
-	this->size = size;
+    this->size = size;
 
-	setup();
+    setup();
 }
 
-void GridFlea::reset()
-{
-	if (isDead())
-	{
-		throw invalid_argument("Can not reset a Dead (deactivated) GridFlea");
-	}
+void GridFlea::reset() {
+    if (isDead()) {
+        throw invalid_argument("Can not reset a Dead (deactivated) GridFlea");
+    }
 
-	setup();
+    setup();
 }
 
-void GridFlea::revive(unsigned int energy)
-{
-	if (!isInactive())
-	{
-		throw invalid_argument("Can not revive an Active or Dead (deactivated) GridFlea");
-	}
+void GridFlea::revive(unsigned int energy) {
+    if (!isInactive()) {
+        throw invalid_argument("Can not revive an Active or Dead (deactivated) GridFlea");
+    }
 
-	this->energy = energy;
-	state = Active;
+    this->energy = energy;
+    state = Active;
 }
 
-void GridFlea::move(int p)
-{
-	if (isDead())
-	{
-		throw invalid_argument("Can not move a Dead (deactivated) GridFlea");
-	}
+void GridFlea::move(int p) {
+    if (isDead()) {
+        throw invalid_argument("Can not move a Dead (deactivated) GridFlea");
+    }
 
-	int amount = (isEnergetic() || p == 0) ? p : UNENERGETIC_MOVE_AMT;
+    int amount = (isEnergetic() || p == 0) ? p : UNENERGETIC_MOVE_AMT;
 
-	if (direction == X)
-	{
-		if (abs(x) > BOUND_X - abs(amount))
-		{
-			state = Dead;
-		}
-		x += amount;
-	}
-	else
-	{
-		if (abs(y) > BOUND_Y - abs(amount))
-		{
-			state = Dead;
-		}
-		y += amount;
-	}
+    if (direction == X) {
+        if (abs(x) > BOUND_X - abs(amount)) {
+            state = Dead;
+        }
+        x += amount;
+    } else {
+        if (abs(y) > BOUND_Y - abs(amount)) {
+            state = Dead;
+        }
+        y += amount;
+    }
 
-	switchDirection();
+    switchDirection();
 
-	reward -= abs(amount);
-	energy--;
+    reward -= abs(amount);
+    energy--;
 }
 
-int GridFlea::value()
-{
-	if (!isActive())
-	{
-		throw invalid_argument("Can not get value of an Inactive or Dead (deactivated) GridFlea");
-	}
+int GridFlea::value() {
+    if (!isActive()) {
+        throw invalid_argument("Can not get value of an Inactive or Dead (deactivated) GridFlea");
+    }
 
-	return reward * size * getChange();
+    return reward * size * getChange();
 }
 
-GridFlea::State GridFlea::getState()
-{
-	if (state == Active && !isEnergetic())
-	{
-		state = Inactive;
-	}
-	else if (state == Inactive && isEnergetic())
-	{
-		state = Active;
-	} else if (state != Dead && isOutOfBounds())
-	{
-		state = Dead;
-	}
+GridFlea::State GridFlea::getState() {
+    if (state == Active && !isEnergetic()) {
+        state = Inactive;
+    } else if (state == Inactive && isEnergetic()) {
+        state = Active;
+    } else if (state != Dead && isOutOfBounds()) {
+        state = Dead;
+    }
 
-	return state;
+    return state;
 }
 
-bool GridFlea::isActive()
-{
-	return getState() == Active;
-}
-bool GridFlea::isInactive()
-{
-	return getState() == Inactive;
-}
-bool GridFlea::isDead()
-{
-	return getState() == Dead;
+bool GridFlea::isActive() {
+    return getState() == Active;
 }
 
-void GridFlea::setup()
-{
-	x = initX;
-	y = initY;
-	energy = initEnergy;
-	reward = initReward;
-
-	state = Active;
-	direction = X;
+bool GridFlea::isInactive() {
+    return getState() == Inactive;
 }
 
-int GridFlea::getChange() const
-{
-	return abs(initX - x) + abs(initY - y);
+bool GridFlea::isDead() {
+    return getState() == Dead;
 }
 
-bool GridFlea::isOutOfBounds() const
-{
-	return abs(x) > BOUND_X || abs(y) > BOUND_Y;
+void GridFlea::setup() {
+    x = initX;
+    y = initY;
+    energy = initEnergy;
+    reward = initReward;
+
+    state = Active;
+    direction = X;
 }
 
-bool GridFlea::isEnergetic() const
-{
+int GridFlea::getChange() const {
+    return abs(initX - x) + abs(initY - y);
+}
+
+bool GridFlea::isOutOfBounds() const {
+    return abs(x) > BOUND_X || abs(y) > BOUND_Y;
+}
+
+bool GridFlea::isEnergetic() const {
     return energy > 0;
 }
 
-void GridFlea::switchDirection()
-{
-	direction = (direction == X) ? Y : X;
+void GridFlea::switchDirection() {
+    direction = (direction == X) ? Y : X;
 }
