afc21cfe0c5cd91c70bf0049c85d05d624dd981b
Gary Tou <gary@garytou.com>
2022-04-14 21:16:33 -0700

    GridFlea: add active bound and invariants/conditionsC

--- P2/GridFlea.cpp
+++ P2/GridFlea.cpp
@@ -3,24 +3,55 @@
 // CPSC 3200, P2
 
 // Revision History:
-// - April 6th, 2022: Port GridFlea from C# to C++
-// - April 12th, 2022: Add an isEnergetic query method to separate some state logic from Active/Inactive/Dead
+// - P2. April 6th, 2022: Port GridFlea from C# to C++
+// - P2. April 12th, 2022: Add an `isEnergetic()` query method to separate some state
+//     logic from Active/Inactive/Dead
+// - P2. April 14th, 2022: Add an `activeBound` to completely separate energy and
+//     state
+
+// =============================================================================
+// ----------------------------- CLASS INVARIANTS ------------------------------
+// =============================================================================
+// This GridFlea class represents a flea which can `move()` within a cartesian
+// grid — either along the X or Y axis. The direction (Axis) of travel
+// alternates per `move()` and defaults to the X axis on initialization.
+//
+// Throughout the lifetime of a GridFlea, it can either in an Active, Inactive,
+// or Dead (deactivated) state. Its State largely depends on the position of the
+// GridFlea.
+//
+// Error Handling for GridFlea is done through throwing Exceptions.
 
 #include <stdexcept>
 #include <cmath>
 #include "GridFlea.h"
 
-GridFlea::GridFlea(int x, int y, unsigned int size, int reward, int energy) {
+/*
+ * Preconditions: none
+ * Postconditions:
+ *   - An Active GridFlea
+ *   - An Inactive GridFlea (if initial x/y is outside of active bound). Active
+ *     Bound is provided as an argument to the constructor.
+ *   - A Dead GridFlea (if initial x/y is outside of grid bounds)
+ */
+GridFlea::GridFlea(int x, int y, unsigned int activeBound, unsigned int size,
+                   int reward, int energy) {
     initX = x;
     initY = y;
     initEnergy = energy;
     initReward = reward;
 
     this->size = size;
+    this->activeBound = activeBound;
 
     setup();
 }
 
+/*
+ * Preconditions: GridFlea must NOT be Dead
+ * Postconditions: Same state as when the GridFlea was initialized. See GridFlea
+ *   constructor for more information (above).
+ */
 void GridFlea::reset() {
     if (isDead()) {
         throw invalid_argument("Can not reset a Dead (deactivated) GridFlea");
@@ -29,18 +60,30 @@ void GridFlea::reset() {
     setup();
 }
 
+/*
+ * Preconditions: GridFlea is NOT Dead
+ * Postconditions: none (GridFlea remains in the same state)
+ */
 void GridFlea::revive(unsigned int energy) {
-    if (!isInactive()) {
-        throw invalid_argument("Can not revive an Active or Dead (deactivated) GridFlea");
+    if (isDead()) {
+        throw invalid_argument("Can not revive a Dead (deactivated) GridFlea");
     }
 
     this->energy = (int) energy;
-    state = Active;
 }
 
+/*
+ * Preconditions: GridFlea is Active
+ * Postconditions:
+ *   - GridFlea is Active (if x/y are both on or within the grid bound)
+ *   - GridFlea is Inactive (if either x/y is between the active bound and grid
+ *     bound)
+ *   - GridFlea is Dead (if either x/y is outside the grid bound)
+ */
 void GridFlea::move(int p) {
     if (!isActive()) {
-        throw invalid_argument("Can not move an Inactive or Dead (deactivated) GridFlea");
+        throw invalid_argument(
+                "Can not move an Inactive or Dead (deactivated) GridFlea");
     }
 
     int amount = (isEnergetic() || p == 0) ? p : UNENERGETIC_MOVE_AMT;
@@ -63,37 +106,66 @@ void GridFlea::move(int p) {
     energy--;
 }
 
+/*
+ * Preconditions: GridFlea is Active
+ * Postconditions: none (GridFlea remains in the same state)
+ */
 int GridFlea::value() {
     if (!isActive()) {
-        throw invalid_argument("Can not get value of an Inactive or Dead (deactivated) GridFlea");
+        throw invalid_argument(
+                "Can not get value of an Inactive or Dead (deactivated) GridFlea");
     }
 
     return reward * size * getChange();
 }
 
+/*
+ * Preconditions: none
+ * Postconditions: none
+ */
 GridFlea::State GridFlea::getState() {
-    if (state == Active && !isEnergetic()) {
-        state = Inactive;
-    } else if (state == Inactive && isEnergetic()) {
-        state = Active;
-    } else if (state != Dead && isOutOfBounds()) {
+    if (isDead()) return state;
+
+    if (isOutOfGridBounds()) {
         state = Dead;
+    } else if (isOutOfActiveBounds()) {
+        state = Inactive;
+    } else {
+        state = Active;
     }
 
     return state;
 }
 
+/*
+ * Preconditions: none
+ * Postconditions: none
+ */
 bool GridFlea::isActive() {
     return getState() == Active;
 }
 
+/*
+ * Preconditions: none
+ * Postconditions: none
+ */
 bool GridFlea::isInactive() {
     return getState() == Inactive;
 }
 
+/*
+ * Preconditions: none
+ * Postconditions: none
+ */
 bool GridFlea::isDead() {
     return getState() == Dead;
 }
+
+/*
+ * =============================================================================
+ * ----------------------------- PRIVATE METHODS -------------------------------
+ * =============================================================================
+ */
 
 void GridFlea::setup() {
     x = initX;
@@ -109,7 +181,11 @@ int GridFlea::getChange() const {
     return abs(initX - x) + abs(initY - y);
 }
 
-bool GridFlea::isOutOfBounds() const {
+bool GridFlea::isOutOfActiveBounds() const {
+    return abs(x) > activeBound || abs(y) > activeBound;
+}
+
+bool GridFlea::isOutOfGridBounds() const {
     return abs(x) > BOUND_X || abs(y) > BOUND_Y;
 }
 
@@ -120,3 +196,95 @@ bool GridFlea::isEnergetic() const {
 void GridFlea::switchDirection() {
     direction = (direction == X) ? Y : X;
 }
+
+// =============================================================================
+// ------------------------- IMPLEMENTATION INVARIANTS -------------------------
+// =============================================================================
+// The GridFlea's overall state is dependent on the following private variables:
+//   - `x` (and `initX`)
+//   - `y` (and `initY`)
+//   - `size` — This never changes.
+//   - `activeBound` — This never changes.
+//   - `energy` (and `initEnergy`)
+//   - `reward` (and `initReward`)
+//   - `state` — Represented by an enum: Active, Inactive, Dead
+//   - `direction` — Represented by an enum: X, Y
+// All of these variables, expect for `state` and `direction`, are initialized
+// through the constructor and have default values.
+//
+// >>>> STATE TRANSITIONS <<<<
+// All state transitions are delayed until necessary and handled by the
+// `getState()` method — except for a single state transition to Dead in the
+// `move()` method. That state transition if necessary is necessary to handle
+// the edge case of an Integer overflow when a GridFlea tries to move far beyond
+// its bound. This architecture was chosen as it allows for most state
+// transitions rules to be contained in one location (the `getState()` method).
+//
+// The possible states and directions of a GridFlea are defined by the `State`
+// and `Axis` enums, respectively. These enums allow for easy additions of
+// additional states and axes when necessary. The possible states also have
+// syntactic sugar query methods (`isActive()`, `isInactive()`, `isDead()`) to
+// help the client write cleaner code.
+//
+// >>>> ENERGY <<<<
+// In addition to the main state (Active, Inactive, Dead), GridFleas also have
+// a notion of "energy". A GridFlea can either be Energetic or Unenergetic.
+// Whether a GridFlea is Energetic or Unenergetic is determined by its energy
+// value — which decreases per `move()`. When energy <= 0, the GridFlea becomes
+// Unenergetic and remains in that state until its `revive()` is called.
+//
+// >>>> REWARD <<<<<
+// The reward of a GridFlea is an indicator for how far a GridFlea was moved.
+// It begins at an initial value (`initReward`) and decreases by the number of
+// squares moved.
+//
+// >>>> VALUE <<<<
+// The value of a GridFlea is a calculation containing the reward, size, and
+// change in position.
+//
+// >>>> BOUND CHECKING <<<<
+// There are two bounds within a GridFlea: The grid bounds and the active
+// bounds. The grid bounds are fixed for all GridFleas, however, the active
+// bounds (which determines whether a GridFlea is Active or Inactive) can be set
+// via the constructor.
+// A GridFlea is considered to be out of bounds when the absolute value of the
+// current position extends the relevant bound (either grid bound or active =
+// bound).
+//
+// >>>> MOVE() <<<<<
+// Only Active GridFleas can move. A GridFlea can move in one of two directions
+// (X or Y). The GridFlea's facing direction (current direction) is determined
+// by the `direction` (a value of the Direction enum). Upon initialization, the
+// GridFlea is facing the X axis. However, it will alternate between X and Y
+// after every `move()`.
+// The amount of squares moved will be influenced by the argument `p`. An
+// Energetic GridFlea will be allowed to move `p` squares when requested,
+// however, an Unenergetic GridFlea will only be allowed to move 1 square (or 0,
+// if `p` is equal 0). A movement of zero may represent a GridFlea skipping this
+// move cycle — which is possible even if it is Unenergetic.
+// Bounds checking is also performed to ensure that a GridFlea is marked as Dead
+// when it moves beyond the grid bounds. See the Bounds Checking section for
+// more details.
+// After moving, the GridFlea's rewards and energy values are updated. See those
+// sections for more details.
+//
+// >>>> REVIVE() <<<<<
+// Dead GridFleas can NOT be revived. Upon reviving a GridFlea, its energy is
+// set to the value provided by the parameter `energy`. This will bring the
+// GridFlea back to an Energetic state.
+//
+// >>>> RESET() <<<<<
+// Dead GridFleas can NOT be reset. Upon resetting a GridFlea, all member
+// variables are set back to their initial values. For example, `x` is set to
+// `initX`. This means that it will also bring the GridFlea back to its initial
+// state (Active, Inactive, or Dead). If a GridFlea was initialized as Inactive
+// or Dead, it will go back to that state.
+// This `reset()` method utilizes the `setup()` method which is shared by the
+// constructor and performs the setting of current values to initial values.
+//
+// >>>> `GridFlea()` (Constructor) <<<<<
+// The constructor takes in initial values for the GridFlea and saves them to
+// private member variables. Then, it calls the `setup()` method to set the
+// current values to the initial values.
+
+// Error handling is done through throwing Exceptions.
--- P2/GridFlea.h
+++ P2/GridFlea.h
@@ -2,6 +2,53 @@
 // April 6th, 2022
 // CPSC 3200, P2
 
+// =============================================================================
+// ----------------------------- CLASS INVARIANTS ------------------------------
+// =============================================================================
+// This GridFlea class represents a flea which can `move()` within a cartesian
+// grid — either along the X or Y axis. The direction (Axis) of travel
+// alternates per `move()` and defaults to the X axis on initialization.
+//
+// Throughout the lifetime of a GridFlea, it can either in an Active, Inactive,
+// or Dead (deactivated) state. Its State largely depends on the position of the
+// GridFlea.
+//
+// Error Handling for GridFlea is done through throwing Exceptions.
+
+// =============================================================================
+// --------------------------- INTERFACE INVARIANTS ----------------------------
+// =============================================================================
+// `GridFlea()` (Constructor):
+//   - Creates a new GridFlea using the given arguments.
+//   - Supports creation using default arguments
+//   - Allows for creation of GridFleas in an Active, Inactive, or Dead state
+//     since there are scenarios where a client may want an Inactive or Dead
+//     GridFlea right off the bat.
+// `reset()`:
+//   - Resets the GridFlea back to its original state (as defined by the
+//     arguments provided to the constructor).
+//   - You can not reset a Dead GridFlea.
+//   - If a GridFlea was initialized in an Inactive state, resetting
+//     that GridFlea will bring it back to its original Inactive state.
+// `revive()`:
+//   - Provides an Inactive GridFlea with a new energy value.
+//   - You can not revive a Dead GridFlea.
+// `move()`:
+//   - Moves the GridFlea in the direction it is currently facing.
+//   - You can only move an Active GridFlea.
+//   - Moving a GridFlea may result in it becoming Inactive or Dead.
+// `value()`:
+//   - Returns the value of the GridFlea.
+//   - You can only get the value of an Active GridFlea.
+//
+// The GridFlea class provides multiple query methods for retrieving the state
+// of the GridFlea. You can either use the `getState()` method which returns a
+// value from the `State` enum (Active, Inactive, Dead). Or, the GridFlea class
+// also provides boolean methods for each state as syntatic sugar.
+//   - `isActive()`: Whether the GridFlea is in an Active state.
+//   - `isInactive()`: Whether the GridFlea is in an Inactive state.
+//   - `isDead()`: Whether the GridFlea is in a Dead state.
+
 #ifndef GRIDFLEA_H
 #define GRIDFLEA_H
 
@@ -15,22 +62,68 @@ public:
         Dead
     };
 
-    GridFlea(int x = 0, int y = 0, unsigned int size = 10, int reward = 10, int energy = 1);
+    /*
+     * Preconditions: none
+     * Postconditions:
+     *   - An Active GridFlea
+     *   - An Inactive GridFlea (if initial x/y is outside of active bound). Active
+     *     Bound is provided as an argument to the constructor.
+     *   - A Dead GridFlea (if initial x/y is outside of grid bounds)
+     */
+    GridFlea(int x = 0, int y = 0, unsigned int activeBound = 800,
+             unsigned int size = 10, int reward = 10, int energy = 1);
 
+    /*
+     * Preconditions: GridFlea must NOT be Dead
+     * Postconditions: Same state as when the GridFlea was initialized. See GridFlea
+     *   constructor for more information (above).
+     */
     void reset();
 
+    /*
+     * Preconditions: GridFlea is NOT Dead
+     * Postconditions: none (GridFlea remains in the same state)
+     */
     void revive(unsigned int energy);
 
+    /*
+     * Preconditions: GridFlea is Active
+     * Postconditions:
+     *   - GridFlea is Active (if x/y are both on or within the grid bound)
+     *   - GridFlea is Inactive (if either x/y is between the active bound and grid
+     *     bound)
+     *   - GridFlea is Dead (if either x/y is outside the grid bound)
+     */
     void move(int p);
 
+    /*
+     * Preconditions: GridFlea is Active
+     * Postconditions: none (GridFlea remains in the same state)
+     */
     int value();
 
+    /*
+     * Preconditions: none
+     * Postconditions: none
+     */
     State getState();
 
+    /*
+     * Preconditions: none
+     * Postconditions: none
+     */
     bool isActive();
 
+    /*
+     * Preconditions: none
+     * Postconditions: none
+     */
     bool isInactive();
 
+    /*
+     * Preconditions: none
+     * Postconditions: none
+     */
     bool isDead();
 
 private:
@@ -39,6 +132,7 @@ private:
     static const int UNENERGETIC_MOVE_AMT = 1;
 
     unsigned int size;
+    unsigned int activeBound;
 
     int initX;
     int initY;
@@ -64,7 +158,9 @@ private:
 
     int getChange() const;
 
-    bool isOutOfBounds() const;
+    bool isOutOfActiveBounds() const;
+
+    bool isOutOfGridBounds() const;
 
     bool isEnergetic() const;
 
