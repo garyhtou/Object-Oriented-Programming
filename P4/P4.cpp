777463f3c940c95a8798f6ff36867bb03e1fd9d3
Gary Tou <gary@garytou.com>
2022-05-15 04:24:59 -0700

    Use smart pointers in driver

--- P4/P4.cpp
+++ P4/P4.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
 #include "inFest.h"
+#include <vector>
+#include <memory>
 
 using namespace std;
 
-const int NUM_INFESTS = 10;
-const string PRINT_PREFIX = "|  > ";
+const int NUM_SHARED_INFESTS = 5;
+const string PRINT_PREFIX = "|  ";
 const string CLONED_PREFIX = PRINT_PREFIX + "  Cloned Infest:         ";
 const string ORIGINAL_PREFIX = PRINT_PREFIX + "  Original Infest:       ";
-const string CBV_PREFIX = PRINT_PREFIX + "  Copy by value Infest:  ";
 const int MOVE_LIMIT = 20;
 const int SEVERITY_LIMIT = 100;
 
-Infest **createInfests(int num) {
-    Infest **infests = new Infest *[num];
+unique_ptr<Infest> printInitStats(unique_ptr<Infest> infest) {
+    cout << "  It was created with a min value of " << infest->minValue()
+         << " and a max value of " << infest->maxValue() << "." << endl;
+    return infest;
+}
+
+unique_ptr<Infest> createInfest(int id) {
+    cout << "Creating Infest #" << id
+         << ". Transferring sole ownership to caller." << endl;
+    unsigned int severity = rand() % SEVERITY_LIMIT + 1;
+    return make_unique<Infest>(severity);
+}
+
+
+vector<shared_ptr<Infest>> createInfestCollection(int num) {
+    vector<shared_ptr<Infest>> infests = vector<shared_ptr<Infest>>();
     for (int i = 0; i < num; i++) {
-        unsigned int severity = rand() % SEVERITY_LIMIT + 1;
-        infests[i] = new Infest(severity);
+        unique_ptr<Infest> infest = createInfest(i + 1);
+        infest = printInitStats(move(infest));
+
+        // Convert to shared pointer
+        shared_ptr<Infest> sharedInfest = move(infest);
+        infests.push_back(sharedInfest);
     }
     return infests;
 }
 
-void deleteInfests(Infest **infests) {
-    for (int i = 0; i < NUM_INFESTS; i++) {
-        delete infests[i];
-    }
-    delete[] infests;
-}
-
-void move(Infest &infest, unsigned int times, string type = "") {
+void move(shared_ptr<Infest> infest, unsigned int times, string type = "") {
     for (int i = 0; i < times; i++) {
         int amount = rand() % MOVE_LIMIT - (MOVE_LIMIT / 2);
-        infest.move(amount);
+        infest->move(amount);
     }
     cout << PRINT_PREFIX << "Moved ";
     if (type.length() > 0) {
@@ -39,22 +51,20 @@ void move(Infest &infest, unsigned int times, string type = "") {
     cout << times << " times" << endl;
 }
 
-void moveBoth(Infest &infest1, Infest &infest2, unsigned int times) {
+void moveBoth(shared_ptr<Infest> &infest1, shared_ptr<Infest> &infest2,
+              unsigned int times) {
     for (int i = 0; i < times; i++) {
         int amount = rand() % (int) (MOVE_LIMIT / 2) - (int) (MOVE_LIMIT / 4);
-        infest1.move(amount);
-        infest2.move(amount);
+        infest1->move(amount);
+        infest2->move(amount);
     }
-    cout << PRINT_PREFIX << "Moved both Infests " << times << " times" << endl;
+    cout << PRINT_PREFIX << "Moved both Infests the same amount " << times
+         << " times" << endl;
 }
 
-void printVals(Infest &infest, string prefix) {
-    cout << prefix << "Value. Min: " << infest.minValue() << "\t Max: "
-         << infest.maxValue() << endl;
-}
-
-void printValsCopyByValue(Infest infest, string prefix) {
-    printVals(infest, prefix);
+void printVals(shared_ptr<Infest> infest, string prefix) {
+    cout << prefix << "Value. Min: " << infest->minValue() << "\t Max: "
+         << infest->maxValue() << endl;
 }
 
 const int MOVE1_TIMES = 5;
@@ -62,8 +72,8 @@ const int MOVE2_TIMES = 3;
 const int MOVE3_TIMES = 5;
 const int MOVE4_TIMES = 2;
 
-void run(Infest &infest, int id) {
-    cout << "======= Running Infest #" << id << " =======" << endl;
+void run(shared_ptr<Infest> &infest, int id) {
+    cout << "\n======= Running Infest #" << id << " =======" << endl;
 
     move(infest, MOVE1_TIMES);
     printVals(infest, PRINT_PREFIX);
@@ -71,7 +81,7 @@ void run(Infest &infest, int id) {
     move(infest, MOVE2_TIMES);
     printVals(infest, PRINT_PREFIX);
 
-    Infest clonedInfest = infest;
+    shared_ptr<Infest> clonedInfest = infest;
     cout << PRINT_PREFIX << endl;
     cout << PRINT_PREFIX << "Created a copy of Infest #" << id
          << " (Cloned Infest)" << endl;
@@ -88,17 +98,22 @@ void run(Infest &infest, int id) {
     move(clonedInfest, MOVE4_TIMES, "Cloned");
     printVals(clonedInfest, ORIGINAL_PREFIX);
 
-    printValsCopyByValue(clonedInfest, CBV_PREFIX);
-
-    cout << "=== Finished Running Infest #" << id << " ===\n\n" << endl;
+    cout << "=== Finished Running Infest #" << id << " ===\n" << endl;
 }
 
 int main(int argc, char *argv[]) {
-    Infest **infests = createInfests(NUM_INFESTS);
+    vector<shared_ptr<Infest>> infests = createInfestCollection(
+            NUM_SHARED_INFESTS);
 
-    for (int i = 0; i < NUM_INFESTS; i++) {
-        run(*infests[i], i + 1);
+    for (int i = 0; i < infests.size(); i++) {
+        run(infests.at(i), i + 1);
     }
 
-    deleteInfests(infests);
+    // Remove all Infests from the vector
+    int count = 0;
+    while (!infests.empty()) {
+        cout << "Removing Infest #" << count + 1 << " from vector" << endl;
+        infests.pop_back();
+        count++;
+    }
 }
