c2814a5a29c7dcbcfdc9035bf193cc2a63036ef4
Gary Tou <gary@garytou.com>
2022-06-01 01:12:12 -0700

    finish Guard and SkipGuard

---
+++ P5/P5/Guard.cs
@@ -0,0 +1,113 @@
+namespace Guards;
+
+public enum Mode
+{
+    UP,
+    DOWN
+}
+
+public class Guard
+{
+    public Guard(int[] s)
+    {
+        if (s == null)
+        {
+            throw new Exception("s array can not be null");
+        }
+
+        if (s.Length <= 0)
+        {
+            throw new Exception("s array can not be null");
+        }
+
+        // Copy data from argument array
+        sArray = new int[] { };
+        foreach (var val in s)
+        {
+            AppendToArray(ref sArray, val);
+        }
+    }
+
+    public virtual int Value(uint x)
+    {
+        return ValueHelper(x, sArray);
+    }
+
+    public void Toggle()
+    {
+        mode = IsUp() ? Mode.DOWN : Mode.UP;
+    }
+
+    public bool IsUp()
+    {
+        return mode == Mode.UP;
+    }
+
+    public bool IsDown()
+    {
+        return mode == Mode.DOWN;
+    }
+
+    private Mode mode = Mode.UP;
+    protected readonly int[] sArray;
+
+    protected int ValueHelper(uint x, int[] arr)
+    {
+        int start = IsUp() ? 0 : arr.Length - 1;
+        int end = IsUp() ? arr.Length : -1;
+
+        int prime = 0;
+        for (int i = start; i < end; i++)
+        {
+            int val = arr[i];
+            if (
+                (IsUp() && val <= x) ||
+                (IsDown() && val >= x)
+            )
+            {
+                continue;
+            }
+
+            if (!IsPrime(val)) continue;
+
+            // Save the smallest/largest prime
+            if (
+                (IsUp() && val < prime) ||
+                (IsDown() && val > prime)
+            )
+            {
+                prime = val;
+            }
+        }
+
+        return prime;
+    }
+
+    private static bool IsPrime(int val)
+    {
+        if (val < 2) return false;
+
+        for (int i = 2; i < val; i++)
+        {
+            if (val % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    protected static void AppendToArray(ref int[] array, int val)
+    {
+        int[] newArr = new int[array.Length + 1];
+        for (int i = 0; i < array.Length; i++)
+        {
+            newArr[i] = array[i];
+        }
+
+        newArr[array.Length] = val;
+
+        array = newArr;
+    }
+}
---
+++ P5/P5/SkipGuard.cs
@@ -0,0 +1,31 @@
+namespace Guards;
+
+public class SkipGuard : Guard
+{
+    public SkipGuard(int[] s) : base(s)
+    {
+    }
+
+    public override int Value(uint x)
+    {
+        int k = GetK();
+
+        int[] kArray = { };
+        for (int i = 0; i < sArray.Length; i++)
+        {
+            if (i % k == 0) continue;
+            AppendToArray(ref kArray, sArray[i]);
+        }
+
+        return ValueHelper(x, kArray);
+    }
+
+
+    private int kIndex = 0;
+
+    private int GetK()
+    {
+        kIndex++;
+        return sArray[kIndex % sArray.Length];
+    }
+}
